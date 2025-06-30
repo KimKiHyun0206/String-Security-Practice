5fa8bca39f141959482c9a707526907d23de6cca
Duskafka <rlarlgus0206@naver.com>
2025-06-30 19:36:18 +0900

    update: 로그인하면 로그인된 것을 알 수 있도록 ID를 브라우저에 띄움

--- src/main/java/com/springsecuritypractice/UIController/UIController.java
+++ src/main/java/com/springsecuritypractice/UIController/UIController.java
@@ -1,10 +1,18 @@
 package com.springsecuritypractice.UIController;
 
+import com.springsecuritypractice.student.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UIController {
+
+    private final StudentService studentService;
 
     @RequestMapping
     public String home() {
@@ -14,5 +22,14 @@ public class UIController {
     @RequestMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @RequestMapping("/name")
+    public String name(
+            @AuthenticationPrincipal UserDetails userDetails,
+            Model model
+    ) {
+        model.addAttribute("name", userDetails.getUsername());
+        return "name";
     }
 }
