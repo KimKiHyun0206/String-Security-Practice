83b28d5b9948bfc236633b82923e9b3d5b543866
Duskafka <rlarlgus0206@naver.com>
2025-07-01 18:24:17 +0900

    fix: import문 수정으로 보터와 필터를 검증함

--- src/main/java/com/springsecuritypractice/SpringSecurityPracticeApplication.java
+++ src/main/java/com/springsecuritypractice/SpringSecurityPracticeApplication.java
@@ -1,18 +1,16 @@
 package com.springsecuritypractice;
 
-import com.springsecuritypractice.config.JwtSecurityConfig;
-import com.springsecuritypractice.config.SessionSecurityConfig;
+import com.springsecuritypractice.config.filter.JwtCustomFilterAndVoterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import(SessionSecurityConfig.class)
-@Import(JwtSecurityConfig.class)
+//@Import(JwtSecurityConfig.class)
+@Import(JwtCustomFilterAndVoterConfig.class)
 public class SpringSecurityPracticeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityPracticeApplication.class, args);
     }
-
 }
