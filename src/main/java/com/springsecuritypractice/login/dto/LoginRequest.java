f175800cbef0659dacd0cb4c3b2b46052fb5f127
Duskafka <rlarlgus0206@naver.com>
2025-07-01 15:41:45 +0900

    fix: 테스트에서도 사용할 수 있도록 AllArgsConstructor사용

--- src/main/java/com/springsecuritypractice/login/dto/LoginRequest.java
+++ src/main/java/com/springsecuritypractice/login/dto/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.springsecuritypractice.login.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginRequest {
     private String loginId;
     private String password;
--- src/main/java/com/springsecuritypractice/student/dto/request/StudentCreateRequest.java
+++ src/main/java/com/springsecuritypractice/student/dto/request/StudentCreateRequest.java
@@ -1,8 +1,10 @@
 package com.springsecuritypractice.student.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class StudentCreateRequest {
     private String loginId;
     private String password;
