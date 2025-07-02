faee76362237e269277fdb0d757c76ca3d9205dc
Duskafka <rlarlgus0206@naver.com>
2025-07-02 11:40:32 +0900

    refactor: 원래 Voter까지 한 번에 처리하였는데 Voter는 분리하고 커스텀 필터만 설정하도록 변경함

--- src/main/java/com/springsecuritypractice/config/filter/JwtCustomFilterConfig.java
+++ src/main/java/com/springsecuritypractice/config/filter/JwtCustomFilterConfig.java
@@ -2,29 +2,19 @@ package com.springsecuritypractice.config.filter;
 
 import com.springsecuritypractice.filter.AdminFilter;
 import com.springsecuritypractice.jwt.JwtFilter;
-import com.springsecuritypractice.voter.wrapper.AccessDecisionManagerWrapper;
-import com.springsecuritypractice.voter.CustomIpCheckVoter;
-import com.springsecuritypractice.voter.CustomRoleVoter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.vote.UnanimousBased;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.util.List;
-
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class JwtCustomFilterAndVoterConfig {
+public class JwtCustomFilterConfig {
     private final JwtFilter jwtFilter;
 
     @Bean
@@ -33,42 +23,7 @@ public class JwtCustomFilterAndVoterConfig {
     }
 
     @Bean
-    public CustomIpCheckVoter ipCheckVoter() {
-        return new CustomIpCheckVoter();
-    }
-
-    @Bean
-    public CustomRoleVoter roleVoter() {
-        return new CustomRoleVoter();
-    }
-
-        /**
-     * * UnanimousBased: 전원 찬성해야 허용
-     *
-     * <br>
-     *
-     * * ConsensusBased: 찬성 다수면 허용
-     *
-     * <br>
-     *
-     * * AffirmativeBased: 하나라도 허용하면 허용
-     * */
-    @Bean
-    public AccessDecisionManager  accessDecisionManager() {
-        return new UnanimousBased(List.of(
-                ipCheckVoter(),
-                roleVoter()
-        ));
-    }
-
-    @Bean
-    public AuthorizationManager<RequestAuthorizationContext> accessDecisionManagerWrapper() {
-        return new AccessDecisionManagerWrapper(accessDecisionManager());
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -81,13 +36,13 @@ public class JwtCustomFilterAndVoterConfig {
                                 "/login/**",
                                 "/**"
                         ).permitAll()
-                        .requestMatchers("/api/admin/**").access(accessDecisionManagerWrapper())
+                        // 인가 관련 별도 설정 없음
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
-        // 커스텀 필터 등록 (UsernamePasswordAuthenticationFilter 앞에 삽입)
-        http.addFilterBefore(myCustomFilter(), UsernamePasswordAuthenticationFilter.class);
+                // JWT 필터 등록
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                // 커스텀 필터 등록 (AdminFilter)
+                .addFilterBefore(myCustomFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
