d787862c143bd2906b8d1bfbb207c831acb52a65
Duskafka <rlarlgus0206@naver.com>
2025-07-03 11:17:04 +0900

    feat: EnableScheduling을 사용하여 스케쥴링 기능 활성화

--- src/main/java/com/springsecuritypractice/SpringSecurityPracticeApplication.java
+++ src/main/java/com/springsecuritypractice/SpringSecurityPracticeApplication.java
@@ -1,14 +1,16 @@
 package com.springsecuritypractice;
 
-import com.springsecuritypractice.config.filter.JwtCustomFilterAndVoterConfig;
+import com.springsecuritypractice.config.filter.JwtCustomFilterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling   // 스케쥴링 기능 활성화
 @SpringBootApplication
 //@Import(SessionSecurityConfig.class)
 //@Import(JwtSecurityConfig.class)
-@Import(JwtCustomFilterAndVoterConfig.class)
+@Import(JwtCustomFilterConfig.class)
 public class SpringSecurityPracticeApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityPracticeApplication.class, args);
