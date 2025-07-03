6cd6e5026469c606a14f2c145d02a9c8369f168f
Duskafka <rlarlgus0206@naver.com>
2025-07-03 11:15:53 +0900

    feat(CSRF): CSRF 토큰이 시간이 지나면 삭제되도록 함

--- src/main/java/com/springsecuritypractice/csrf/CsrfTokenJpaRepository.java
+++ src/main/java/com/springsecuritypractice/csrf/CsrfTokenJpaRepository.java
@@ -3,6 +3,9 @@ package com.springsecuritypractice.csrf;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface CsrfTokenJpaRepository extends JpaRepository<CsrfTokenEntity, String> {
+    void deleteByCreatedAtBefore(LocalDateTime threshold);
 }
